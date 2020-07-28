cbfa2b039fb178685598d367aa4bb6017adb9f2e
frescobar <frescobar87@gmail.com>
2020-07-27 19:26:08 -0500

    format the index file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 
 require("./db/mongoose");
-
 const userRouter = require("./routes/user");
 const taskRouter = require("./routes/task");
 
